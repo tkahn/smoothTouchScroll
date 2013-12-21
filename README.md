d72e1003fc4537dd720d74c2948cc2a50a297e8d
Thomas Kahn <thomas@karnhuset.net>
2013-12-21 20:13:32 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,133 @@
+# jQuery Smooth Touch Scroll
+by Thomas Kahn 
+
+### A lightweight touch version of Smooth Div Scroll
+
+You may have used Smooth Div Scroll to present smooth scrolling content on your webpage. If Smooth Div Scroll is the Swiss Army Knife of smooth scrolling, Smooth Touch Scroll is the lightweight cousin, slimmed down and geared towards touch scrolling. Smooth Touch Scroll will still work on desktop computers that don't have touch (using drag scrolling) but it's primarily intended for touch devices.
+
+## Demo
+I'm currently working on building a demo and documentation page for Smooth Touch Scroll. I apologise it it's not that elaborate yet, but I hope it's enough to get you started together with the documentation here on GitHub.
+
+[Visit the demo page (smoothtouchscroll.com)](http://smoothtouchscroll.com)
+
+## Usage
+
+1. Include jQuery:
+
+	```
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	```
+2. Include jQuery UI containing only the widget functionality:
+
+	```html
+	<script src="js/jquery-ui-1.10.3.custom.min.js"></script>
+	```
+If you need more from jQuery UI than just the widget functionality you can use [the dowload builder at jqueryui.com](http://jqueryui.com/download/). Otherwise the only part from jQuery UI that is required by Smooth Touch Scroll is the Widget functionality. It's included in the download.
+
+2. Include the minified version of jQuery Kinetic:
+
+	```html
+	<script src="js/jquery.kinetic.min.js.min.js"></script>
+	```
+Kinetic is included in the download. If you want to know more, you'll find [all the information here](http://davetayls.me/jquery.kinetic/).
+
+3. Include the minified version Smooth Touch Scroll:
+
+	```html
+	<script src="js/jquery.smoothTouchScroll.min.js"></script>
+	```
+
+3. Call the plugin:
+
+	```html
+	$("#element").smoothTouchScroll();
+	```
+
+## Options
+This is an early version of the plugin with just the bare-bones functionality. Therefore the number of options are limited to begin with.
+
+```javascript
+// Default options
+options: {
+	scrollableAreaClass: "scrollableArea", // String
+	scrollWrapperClass: "scrollWrapper", // String
+	continuousScrolling: false, // Boolean
+	startAtElementId: "", // String
+},
+
+```
+
+### scrollableAreaClass (string)
+If you have added a __scrollable area__ element of your own with a custom class, you can tell the plugin the name of this class here. Normally Smooth Touch Scroll takes care of this automatically, so in most cases you won't be using this option. But if you should need it, it's there.
+
+### scrollWrapperClass (string)
+If you have added a __scroll wrapper__ element of your own with a custom class, you can tell the plugin the name of this class here. Normally Smooth Touch Scroll takes care of this automatically, so in most cases you won't be using this option. But if you should need it, it's there.
+
+### continuousScrolling (boolean)
+Set this option to true if you want endless scrolling in both directions. Smooth Touch Scroll will swap around the elements inside the scroller to give the illusion of endless/continuous scrolling.
+
+### startAtElementId (string)
+If you want the scroller to start at a certain element, you provide the CSS id of this element as a string. The scroller will then start with this element at the leftmost edge of the scroller, provided there are enough elements in the scroller to scroll that far.
+
+## Public methods
+There are not that many methods yet, but these are the ones you can use for now.
+
+### recalculateScrollableArea
+This method goes through all the elements in the scroller and calculates their combined width. Then it sets the width of the scrollable area to this number. Use it if you add or remove elements in the scroller after it has been initialized. 
+
+You can also use the init-method of the plugin by calling the plugin without arguments. Right now the only thing that happens inside the init-method is that recalculateScrollableArea is invoked.
+
+```javascript
+$("#element").smoothTouchScroll("recalculateScrollableArea");
+```
+or use the init-method:
+```javascript
+$("#element").smoothTouchScroll();
+```
+
+### restoreOriginalElements
+When Smooth Touch Scroll is first created it stores a copy of the original elements inside the scroller. When you call this method these elements are restored and replace all the current elements in the scroller.
+
+```javascript
+$("#element").smoothTouchScroll("restoreOriginalElements");
+```
+### show
+Shows the scroller.
+
+```javascript
+$("#element").smoothTouchScroll("show");
+```
+
+### hide
+Hides the scroller.
+
+```javascript
+$("#element").smoothTouchScroll("hide");
+```
+
+### enable
+Enables the scroller. The scroller is enabled per default, so calling this method is only useful if you have previously disabled it.
+
+```javascript
+$("#element").smoothTouchScroll("enable");
+```
+
+### disable
+Disables the scroller. The result is basically a frozen scroller that you can't interact with.
+
+```javascript
+$("#element").smoothTouchScroll("disable");
+```
+### destroy
+Destroys the instantiated plugin and restores the element to its original state.
+```javascript
+$("#element").smoothTouchScroll("destroy");
+```
+
+## Current development status
+__This is version 1.0 of the plugin.__ Eventhough it's based on [Smooth Div Scroll](http://smoothdivscroll.com/), which has been around for many years and is continuously updated, Smooth Touch Scroll is a new plugin. Therefore I ask that you test it before you use it in any production environment. Please report any issues here on GitHub.
+
+## License
+[GNU General Public License](http://www.gnu.org/licenses/gpl-3.0.html) as published by the Free Software Foundation, either version 3 of the License, or any later version.
+
+This plugin is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
---
+++ js/source/jquery.smoothTouchScroll.js
@@ -0,0 +1,311 @@
+/*
+ * jQuery SmoothTouchScroll
+ *
+ * Copyright (c) 2013 Thomas Kahn
+ * Licensed under the GPL license.
+ *
+ * http://www.smoothtouchscroll.com/
+ *
+ * Depends:
+ * jquery-1.10.2.min.js
+   Please use https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js
+   ...or later
+
+ * jquery.kinetic.min.js
+   Used for scrolling on touch devices.
+   Download the latest version at https://github.com/davetayls/jquery.kinetic
+ *
+ */
+
+;(function ( $, window, document, undefined ) {
+
+    $.widget( "thomaskahn.smoothTouchScroll" , {
+
+        // Default options
+        options: {
+			scrollableAreaClass: "scrollableArea", // String
+			scrollWrapperClass: "scrollWrapper", // String
+			continuousScrolling: false, // Boolean
+			startAtElementId: "", // String
+        },
+
+        // Setup widget
+        _create: function () {
+			var self = this, o = this.options, el = this.element;
+			
+			// Create variables for any existing or not existing 
+			// scroller elements on the page.
+			el.data("scrollWrapper", el.find("." + o.scrollWrapperClass));
+			el.data("scrollableArea", el.find("." + o.scrollableAreaClass));
+			
+			// Check what the DOM looks like
+	
+			// Both the scrollable area and the wrapper are missing
+			if (el.data("scrollableArea").length === 0 && el.data("scrollWrapper").length === 0) {
+				el.wrapInner("<div class='" + o.scrollableAreaClass + "'>").wrapInner("<div class='" + o.scrollWrapperClass + "'>");
+
+				el.data("scrollWrapper", el.find("." + o.scrollWrapperClass));
+				el.data("scrollableArea", el.find("." + o.scrollableAreaClass));
+			}
+			
+			// Only the wrapper is missing
+			else if (el.data("scrollWrapper").length === 0) {
+				el.wrapInner("<div class='" + o.scrollWrapperClass + "'>");
+				el.data("scrollWrapper", el.find("." + o.scrollWrapperClass));
+			}
+			
+			// Only the scrollable area is missing
+			else if (el.data("scrollableArea").length === 0) {
+				el.data("scrollWrapper").wrapInner("<div class='" + o.scrollableAreaClass + "'>");
+				el.data("scrollableArea", el.find("." + o.scrollableAreaClass));
+			}
+			
+			// Create variables in the element data storage.
+			el.data("scrollXPos", 0);
+			el.data("scrollableAreaWidth", 0);
+			el.data("startingPosition", 0);
+			el.data("rightScrollingInterval", null);
+			el.data("leftScrollingInterval", null);
+			el.data("previousScrollLeft", 0);
+			el.data("getNextElementWidth", true);
+			el.data("swapAt", null);
+			el.data("startAtElementHasNotPassed", true);
+			el.data("swappedElement", null);
+			el.data("originalElements", el.data("scrollableArea").children());
+			el.data("visible", true);
+			el.data("enabled", true);
+			el.data("scrollableAreaHeight", el.data("scrollableArea").height());
+			
+			/*****************************************
+			SET UP EVENTS FOR TOUCH SCROLLING
+			*****************************************/
+			if (el.data("enabled")) {
+				// Use jquery.kinetic.js for touch scrolling
+				// All the parameters for jquery.kinetic can be
+				// altered here. 
+				// TO-DO: expose the relevant parameters as parameters
+				// of Smooth Touch Scroll.
+				el.data("scrollWrapper").kinetic({
+					cursor: 'move',
+					decelerate: true,
+					triggerHardware: false,
+					y: false,
+					x: true,
+					axisTolerance: 7,
+					slowdown: 0.9,
+					maxvelocity: 120,
+					throttleFPS: 60,
+					moved: function (settings) {
+						if (o.continuousScrolling) {
+							if (el.data("scrollWrapper").scrollLeft() <= 0) {
+								self._checkContinuousSwapLeft();
+							} else {
+								self._checkContinuousSwapRight();
+							}
+						}
+						
+						// Callback
+						self._trigger("touchMoved");
+					},
+					stopped: function (settings) {
+						// Callback
+						self._trigger("touchStopped");
+					}
+				});
+			}
+			
+			/*****************************************
+			SET UP EVENT FOR RESIZING THE BROWSER WINDOW
+			*****************************************/
+			$(window).bind("resize", function () {
+				self._trigger("windowResized");
+			});
+			
+        },
+		/**********************************************************
+		_init 
+		**********************************************************/
+		// This code is run every time the widget is called without arguments.
+		// Use it when you add elements to the scroller without reloading the page.
+		 _init: function () {
+			var self = this, el = this.element;
+		 
+			// Recalculate the total width of the elements inside the scrollable area
+			self.recalculateScrollableArea();
+		
+			// Trigger callback
+			self._trigger("initializationComplete");
+		},
+		/**********************************************************
+		Recalculate the scrollable area
+		**********************************************************/
+		recalculateScrollableArea: function () {
+
+			var tempScrollableAreaWidth = 0, foundStartAtElement = false, o = this.options, el = this.element;
+
+			// Add up the total width of all the items inside the scrollable area
+			el.data("scrollableArea").children().each(function () {
+				// Check to see if the current element in the loop is the one where the scrolling should start
+				if ((o.startAtElementId.length > 0) && (($(this).attr("id")) === o.startAtElementId)) {
+					el.data("startingPosition", tempScrollableAreaWidth);
+					foundStartAtElement = true;
+				}
+				tempScrollableAreaWidth = tempScrollableAreaWidth + $(this).outerWidth(true);
+
+			});
+
+			// If the element with the ID specified by startAtElementId
+			// is not found, reset it
+			if (!(foundStartAtElement)) {
+				el.data("startAtElementId", "");
+			}
+
+			// Set the width of the scrollable area
+			el.data("scrollableAreaWidth", tempScrollableAreaWidth);
+			el.data("scrollableArea").width(el.data("scrollableAreaWidth"));
+
+			// Move to the starting position
+			el.data("scrollWrapper").scrollLeft(el.data("startingPosition"));
+			el.data("scrollXPos", el.data("startingPosition"));
+		},
+		/**********************************************************
+		Check Continuos Swap Right
+		**********************************************************/
+		_checkContinuousSwapRight: function () {
+			var el = this.element, o = this.options;
+
+			// Get the width of the first element. When it has scrolled out of view,
+			// the element swapping should be executed. A true/false variable is used
+			// as a flag variable so the swapAt value doesn't have to be recalculated
+			// in each loop.  
+			if (el.data("getNextElementWidth")) {
+
+				if ((o.startAtElementId.length > 0) && (el.data("startAtElementHasNotPassed"))) {
+					// If the user has set a certain element to start at, set swapAt 
+					// to that element width. This happens once.
+					el.data("swapAt", $("#" + o.startAtElementId).outerWidth(true));
+					el.data("startAtElementHasNotPassed", false);
+				}
+				else {
+					// Set swapAt to the first element in the scroller
+					el.data("swapAt", el.data("scrollableArea").children(":first").outerWidth(true));
+				}
+				el.data("getNextElementWidth", false);
+			}
+
+
+			// Check to see if the swap should be done
+			if (el.data("swapAt") <= el.data("scrollWrapper").scrollLeft()) {
+				el.data("swappedElement", el.data("scrollableArea").children(":first").detach());
+				el.data("scrollableArea").append(el.data("swappedElement"));
+				var wrapperLeft = el.data("scrollWrapper").scrollLeft();
+				el.data("scrollWrapper").scrollLeft(wrapperLeft - el.data("swappedElement").outerWidth(true));
+				el.data("getNextElementWidth", true);
+			}
+		},
+		/**********************************************************
+		Check Continuos Swap Left
+		**********************************************************/
+		_checkContinuousSwapLeft: function () {
+			var el = this.element, o = this.options;
+
+			// Get the width of the first element. When it has scrolled out of view,
+			// the element swapping should be executed. A true/false variable is used
+			// as a flag variable so the swapAt value doesn't have to be recalculated
+			// in each loop.
+
+			if (el.data("getNextElementWidth")) {
+				if ((o.startAtElementId.length > 0) && (el.data("startAtElementHasNotPassed"))) {
+					el.data("swapAt", $("#" + o.startAtElementId).outerWidth(true));
+					el.data("startAtElementHasNotPassed", false);
+				}
+				else {
+					el.data("swapAt", el.data("scrollableArea").children(":first").outerWidth(true));
+				}
+
+				el.data("getNextElementWidth", false);
+			}
+
+			// Check to see if the swap should be done
+			if (el.data("scrollWrapper").scrollLeft() === 0) {
+       
+				el.data("swappedElement", el.data("scrollableArea").children(":last").detach());
+				el.data("scrollableArea").prepend(el.data("swappedElement"));
+				el.data("scrollWrapper").scrollLeft(el.data("scrollWrapper").scrollLeft() + el.data("swappedElement").outerWidth(true));
+				el.data("getNextElementWidth", true);
+        
+			}
+
+		},
+		/**********************************************************
+		Restore the original elements in the scroller
+		**********************************************************/
+		restoreOriginalElements: function () {
+			var self = this, el = this.element;
+
+			// Restore the original content of the scrollable area
+			el.data("scrollableArea").html(el.data("originalElements"));
+			self.recalculateScrollableArea();
+			self.jumpToElement("first");
+		},
+		/**********************************************************
+		Show the scroller
+		**********************************************************/
+		show: function () {
+			var el = this.element;
+			el.data("visible", true);
+			el.show();
+		},
+		/**********************************************************
+		Hide the scroller
+		**********************************************************/
+		hide: function () {
+			var el = this.element;
+			el.data("visible", false);
+			el.hide();
+		},
+		/**********************************************************
+		Enable the scroller
+		**********************************************************/
+		enable: function () {
+			var el = this.element;
+
+			el.data("scrollWrapper").kinetic('attach');
+			
+			// Set enabled to true
+			el.data("enabled", true);
+		},
+		/**********************************************************
+		Disable the scroller
+		**********************************************************/
+		disable: function () {
+			var self = this, el = this.element;
+
+			el.data("scrollWrapper").kinetic('detach');
+
+			// Set enabled to false
+			el.data("enabled", false);
+		},
+		
+        // Destroy an instantiated plugin and clean up
+        // modifications the widget has made to the DOM
+        destroy: function () {
+			var self = this, el = this.element;
+			
+			// Remove elements created by the scroller
+			el.data("scrollableArea").remove();
+			el.data("scrollWrapper").remove();
+
+			// Restore the original content of the scrollable area
+			el.html(el.data("originalElements"));
+
+            // For UI 1.8, destroy must be invoked from the
+            // base widget
+            $.Widget.prototype.destroy.call(this);
+            // For UI 1.9, define _destroy instead and don't
+            // worry about
+            // calling the base widget
+        }
+    });
+
+})( jQuery, window, document );
